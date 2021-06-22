9f7238b1d73a80fec74556d891131cbac66355c6
Herculino Trotta <netotrotta@gmail.com>
2021-06-21 22:12:13 -0300

    feat: add SplashPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/splash/splash_page.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
 
 void main() {
-  runApp(Container());
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Pay Flow",
+        theme: ThemeData(primaryColor: AppColors.primary),
+        home: SplashPage());
+  }
 }
