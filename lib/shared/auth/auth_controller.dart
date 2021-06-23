8b309db822a62dba3c0e55303a9dbeba2ee5909e
Herculino Trotta <netotrotta@gmail.com>
2021-06-22 22:15:00 -0300

    feat: Add Navigator call to AuthController

--- lib/shared/auth/auth_controller.dart
+++ lib/shared/auth/auth_controller.dart
@@ -1,15 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/login/login_page.dart';
+
 class AuthController {
   bool _isAuthenticated = false;
   var _user;
 
   get user => _user;
 
-  void setUser(var user) {
+  void setUser(BuildContext context, var user) {
     if (user != null) {
-      _isAuthenticated = true;
       _user = user;
+      _isAuthenticated = true;
+
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => HomePage()));
     } else {
       _isAuthenticated = false;
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => LoginPage()));
     }
   }
 }
