d9e6932a28e8158b3513528e136d5d7330b1059d
Herculino Trotta <netotrotta@gmail.com>
2021-06-23 22:15:06 -0300

    feat: Add BarcodeScannerPage

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -4,6 +4,7 @@ import 'package:payflow/modules/home/home_page.dart';
 import 'package:payflow/modules/login/login_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
@@ -12,12 +13,14 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Pay Flow",
-      theme: ThemeData(primaryColor: AppColors.primary),
+      theme: ThemeData(
+          primaryColor: AppColors.primary, primarySwatch: Colors.orange),
       initialRoute: "/splash",
       routes: {
         "/splash": (context) => SplashPage(),
         "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage()
+        "/login": (context) => LoginPage(),
+        "/barcode_scanner": (context) => BarcodeScannerPage(),
       },
     );
   }
